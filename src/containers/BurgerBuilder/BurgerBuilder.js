5d359f74c1ea1b78a8755befc30f6d23985dacaf
KonradMarzec1991 <konrimarzec@gmail.com>
2020-07-05 11:22:03 +0200

    Control button ORDER NOW state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -18,8 +18,17 @@ class BurgerBuilder extends Component {
             cheese: 2,
             meat: 1
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchasable: false
     };
+
+    updatePurchaseState = (ingredients) => {
+        const sumIngredients = Object.values(ingredients)
+            .reduce((a, b) => a + b)
+        if (sumIngredients >= 0) {
+            this.setState({ purchasable: sumIngredients > 0 })
+        }
+    }
 
     addIngredientHandler = type => {
         const oldCount = this.state.ingredients[type];
@@ -35,6 +44,7 @@ class BurgerBuilder extends Component {
             ingredients: updatedIngredients,
             totalPrice: newPrice
         });
+        this.updatePurchaseState(updatedIngredients);
     }
 
     removeIngredientHandler = type => {
@@ -54,6 +64,7 @@ class BurgerBuilder extends Component {
            ingredients: updatedIngredients,
            totalPrice: newPrice
        });
+       this.updatePurchaseState(updatedIngredients);
     }
 
     render() {
@@ -70,6 +81,8 @@ class BurgerBuilder extends Component {
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disableInfo}
+                    purchasable={this.state.purchasable}
+                    price={this.state.totalPrice}
                 />
             </Aux>
         )

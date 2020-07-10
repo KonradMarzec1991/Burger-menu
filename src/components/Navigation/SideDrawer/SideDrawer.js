e1a648e24f11628baa0fc82bd6f6c8e7fb3fb68e
KonradMarzec1991 <konrimarzec@gmail.com>
2020-07-10 22:15:25 +0200

    Rearrange files

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,11 +1,12 @@
 import React from 'react';
+
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
 import Backdrop from '../../UI/Backdrop/Backdrop';
 import Aux from '../../../hoc/Aux/Aux';
 
-const sideDrawer = props => {
+const sideDrawer = ( props ) => {
     let attachedClasses = [classes.SideDrawer, classes.Close];
     if (props.open) {
         attachedClasses = [classes.SideDrawer, classes.Open];
@@ -13,12 +14,12 @@ const sideDrawer = props => {
     return (
         <Aux>
             <Backdrop show={props.open} clicked={props.closed}/>
-             <div className={attachedClasses.join(' ')}>
+            <div className={attachedClasses.join(' ')}>
                 <div className={classes.Logo}>
-                    <Logo/>
+                    <Logo />
                 </div>
                 <nav>
-                    <NavigationItems/>
+                    <NavigationItems />
                 </nav>
             </div>
         </Aux>

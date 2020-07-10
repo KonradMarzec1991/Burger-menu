96e0e9b16f0177146ccbf7cf627e5025d54c6d17
KonradMarzec1991 <konrimarzec@gmail.com>
2020-07-10 20:11:13 +0200

    Change OrderSummary / Modal on class components

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,32 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = props => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
             return <li key={igKey}>
                 <span style={{textTransform: 'capitalize'}}>{igKey}</span>
-                : {props.ingredients[igKey]}
+                : {this.props.ingredients[igKey]}
             </li>
         })
-    return (
-        <Aux>
-            <h3>Your order</h3>
-            <p>Delicious burger with following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button
-                btnType="Danger"
-                clicked={props.purchaseCancelHandler}>CANCEL</Button>
-            <Button
-                btnType="Success"
-                clicked={props.purchaseContinueHandler}>CONTINUE</Button>
-        </Aux>
-    )
+
+        return (
+            <Aux>
+                <h3>Your order</h3>
+                <p>Delicious burger with following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p>
+                    <strong>Total Price: {this.props.price.toFixed(2)}</strong>
+                </p>
+                <p>Continue to Checkout?</p>
+                <Button
+                    btnType="Danger"
+                    clicked={this.props.purchaseCancelHandler}>CANCEL</Button>
+                <Button
+                    btnType="Success"
+                    clicked={this.props.purchaseContinueHandler}
+                >CONTINUE</Button>
+            </Aux>
+        )
+    }
 };
 
-export default orderSummary;
+export default OrderSummary;

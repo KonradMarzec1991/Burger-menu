b9c8dd8a764760408cc8e139b1375ebdc07946ae
KonradMarzec1991 <konrimarzec@gmail.com>
2020-07-11 14:23:12 +0200

    Handle errors

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,8 +5,10 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 import axios from '../../axios-orders';
 import Spinner from "../../components/UI/Spinner/Spinner";
+
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -141,4 +143,4 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);

68b92b2a225adb46fe1040adcdf960a3777f8d39
KonradMarzec1991 <konrimarzec@gmail.com>
2020-08-09 19:21:02 +0200

    Add actions creators for auth

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -2,16 +2,14 @@ export {
     addIngredient,
     removeIngredient,
     initIngredients,
-    fetchIngredientsFailed
 } from './burgerBuilder';
 
 export {
     purchaseBurger,
-    purchaseBurgerSuccess,
-    purchaseBurgerFail,
     purchaseInit,
-    fetchOrderStart,
-    fetchOrderSuccess,
-    fetchOrderFail,
     fetchOrders
 } from './order';
+
+export {
+    auth
+} from './auth';
